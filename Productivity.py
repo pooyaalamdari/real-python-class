49ca808845b54240b03e3d65b498ca3f04a70b29
pooyaalamdari <31381311+pooyaalamdari@users.noreply.github.com>
2022-02-17 22:06:19 +0330

    Create Productivity.py

---
+++ Productivity.py
@@ -0,0 +1,8 @@
+# Productivity = بهره وری 
+class ProductivitySystem:
+    def track(self, employees, hours):
+        print('Tracking Employee Productivity')
+        print('------') 
+        for employee in employees:
+            employee.work(hours)
+        print('')
