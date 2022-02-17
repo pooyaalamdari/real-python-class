e60fd37897bbb3c0c136f57d44e6a61c160a0dfc
pooyaalamdari <31381311+pooyaalamdari@users.noreply.github.com>
2022-02-17 22:05:43 +0330

    Update hr.py

--- hr.py
+++ hr.py
@@ -1,3 +1,5 @@
+import Productivity
+
 class PayrollSystem:
     def calculate_payroll(self, employees):
         print('Calculating Payroll')
@@ -41,17 +43,59 @@ class CommisionEmployee(SalaryEmployee):
         return self.commision + self.weekly_salary
 
 
-salary_employee = SalaryEmployee(1,'john smith', 1500)
-hourly_employee = HourlyEmployee(2,'john Doe', 40, 15)
-commision_employee = CommisionEmployee(3, 'Kevin Bacon', 1000, 250)
-# we can't call parent class (Employee) directly
-# Generic = Employee(4, 'Alex')
+
+class Manager(SalaryEmployee):
+    def work(self, hours):
+        print(f'name: {self.name} - hours: {hours}')
 
 
-payroll_system = PayrollSystem() 
+class Secretary(SalaryEmployee):
+    def work(self, hours):
+        print(f'name: {self.name} - hours: {hours}')
 
-payroll_system.calculate_payroll([ 
-    salary_employee,
-    hourly_employee,
-    commision_employee
-]) 
+class SalesPerson(CommisionEmployee):
+    def work(self, hours):
+        print(f'name: {self.name} - hours: {hours}')
+
+class FactoryWorker(HourlyEmployee):
+    def work(self, hours):
+        print(f'name: {self.name} - hours: {hours}')
+
+
+
+
+
+# salary_employee = SalaryEmployee(1,'john smith', 1500)
+# hourly_employee = HourlyEmployee(2,'john Doe', 40, 15)
+# commision_employee = CommisionEmployee(3, 'Kevin Bacon', 1000, 250)
+
+# payroll_system = PayrollSystem() 
+
+# payroll_system.calculate_payroll([ 
+#     salary_employee,
+#     hourly_employee,
+#     commision_employee
+# ]) 
+
+# if build a employees.py and add it Manager class we write 
+#employees.Manager(...)
+# manager = employees.Manager(1,'john smith', 1500)
+manager =Manager(1,'john smith', 1500)
+secretary = Secretary(2,'john Doe',1200)
+sales_guy = SalesPerson(3, 'Kevin Bacon', 1000, 250)
+factory_worker = FactoryWorker(4, 'Pete Peterson', 40, 15)
+
+employees = [ 
+    manager,
+    secretary,
+    sales_guy,
+    factory_worker
+]
+
+productivity_system = Productivity.ProductivitySystem()
+# IMPORTANT: it is run before PayrollSystem class 
+productivity_system.track(employees, 40)
+
+
+payroll_system = PayrollSystem()
+payroll_system.calculate_payroll(employees)
